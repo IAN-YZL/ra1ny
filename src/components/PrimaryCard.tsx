add83714a4ed13c34c55a05b99bf42e6839b6bd2
1any1n <ian.yin.zhilin@gmail.com>
2022-01-02 18:36:38 +1000

    finish primary card

--- src/components/PrimaryCard.tsx
+++ src/components/PrimaryCard.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 import { FlexWrapper } from './styledComponents'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import { IWeather } from '../api/weather';
 import { getWeatherIconUrl } from './WeatherCardSm';
+import AirOutlinedIcon from '@mui/icons-material/AirOutlined';
+import OpacityOutlinedIcon from '@mui/icons-material/OpacityOutlined';
 
 const LocationText = styled.p`
 	font-size: 16px;
 	color: #ffffff;
+	margin-left: 2px;
 `
 
 const PrimaryCardWrapper = styled.div`
@@ -47,6 +50,59 @@ const TempText = styled.p`
 	margin: 8px 0;
 `
 
+const WeatherText = styled.p`
+	font-size: 14px;
+	padding: 5px 0;
+`
+
+const BottomWrapper = styled.div`
+	margin: 10px 36px;
+`
+
+const WeatherFeatureLeft = styled.div`
+	display: flex;
+	width: 50%;
+`
+
+const StyledWindIcon = styled(AirOutlinedIcon)`
+	font-size: 16px !important;
+`
+
+const FeatureLabel = styled.p`
+	margin-left: 4px;
+`
+
+const WeatherFeatureWrapper = styled(FlexWrapper)`
+	width: 100%;
+	align-items: center;
+	font-size: 12px;
+	margin-bottom: 4px;
+`
+
+const Divider = styled.div`
+	border: 1px solid #ffffff;
+	height: 10px;
+	margin-right: 17px;
+`
+
+const StyledWaterIcon = styled(OpacityOutlinedIcon)`
+	font-size: 16px !important;
+`
+
+const WeatherFeature = (props: { icon: ReactNode, label: string, value: string }) => {
+	return (
+		<WeatherFeatureWrapper>
+			<WeatherFeatureLeft>
+				{props.icon}
+				<FeatureLabel>{props.label}</FeatureLabel>
+			</WeatherFeatureLeft>
+			<Divider />
+			<div>{props.value}</div>
+		</WeatherFeatureWrapper>
+	)
+}
+
+
 const Location = () => {
 
 	return (
@@ -69,6 +125,19 @@ const PrimaryCard = (props: IWeather) => {
 				<InforDetails>
 					<DateString>{`Today, ${date.toLocaleString(undefined, { day: 'numeric', month: 'long' })}`}</DateString>
 					<TempText>{props.main.temp.toFixed()}<span>&#176;</span></TempText>
+					<WeatherText>{props.weather[0].main}</WeatherText>
+					<BottomWrapper>
+						<WeatherFeature
+							icon={<StyledWindIcon />}
+							label='Wind'
+							value={`${props.wind.speed.toFixed()} km/h`}
+						/>
+						<WeatherFeature
+							icon={<StyledWaterIcon />}
+							label='Hum'
+							value={`${props.main.humidity.toFixed()} %`}
+						/>
+					</BottomWrapper>
 				</InforDetails>
 			</InfoWrapper>
 		</PrimaryCardWrapper>
