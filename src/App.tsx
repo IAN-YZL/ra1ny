6432e4ed42507b17e4570ddc7de2b65f1737b1b1
1any1n <ian.yin.zhilin@gmail.com>
2022-01-03 16:33:15 +1000

    add loading icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import Dashboard from './pages/Dashboard';
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { theme } from './theme';
 import { getCurrentWeather, getForecastWeather, IForecastData, IWeather } from './api/weather';
+import { CircularProgress } from '@mui/material';
 
-// const WeatherContext = createContext<{forecast: IForecastWeather[] | null, current: IWeather | null}>({forecast: null, current: null})
+const LoadingIcon = styled(CircularProgress)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+`
 
 function App() {
   const [forecast, setForecast] = useState<IForecastData>()
   const [current, setCurrent] = useState<IWeather>()
   const [city, setCity] = useState<string>('Sydney')
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
+    setLoading(true)
     async function fetchWeatherAPI() {
       const [forecastRes, currentRes] = await Promise.all([getForecastWeather(city), getCurrentWeather(city)])
       if (!!forecastRes) {
@@ -20,6 +30,7 @@ function App() {
       if (currentRes) {
         setCurrent(currentRes)
       }
+      setLoading(false)
     }
     fetchWeatherAPI()
   }, [city])
@@ -27,13 +38,15 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       {
-        forecast && current ?
-          <Dashboard
-            forecastData={forecast}
-            currentData={current}
-            setCity={value => setCity(value)}
-          /> :
-          <></>
+        !loading ?
+          forecast && current ?
+            <Dashboard
+              forecastData={forecast}
+              currentData={current}
+              setCity={value => setCity(value)}
+            /> :
+            <>Loading Failed!</> :
+          <LoadingIcon />
       }
     </ThemeProvider>
   );
